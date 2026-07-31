51d63e27011a66cb15e1bb87056db6ae5fee20dd
obuen396-afk <obuen396@evit.edu>
2026-07-31 14:17:00 -0700

    Update 1st.py

---
+++ 1st.py
@@ -0,0 +1,20 @@
+
+print("Start!")
+Videogames = False
+print("I made videogames here: ", Videogames)
+
+if Videogames == True :
+    VideoNumbers = 0
+    print("Videogames made: ", VideoNumbers)
+    print("Finish!")
+else :
+    print("Nevermind.") 
+
+print("how do variables add/subtract?")
+x = input("pick number 5!")
+X = int(x)
+if X == 5 :
+    X = X + 1
+else :
+    print("Well, let's move on...  This doesn't work!")
+
