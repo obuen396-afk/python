7c92104d8c47d3a8e80f5bc23e1588bbbc2a97af
obuen396-afk <obuen396@evit.edu>
2026-08-14 13:22:11 -0700

    Update My.py

--- My.py
+++ My.py
@@ -1,8 +1,12 @@
-print("Hello World")
-earningsgoal = input("How much money do you need to save in a year? ")
-months = int(earningsgoal) / 12
-weeks = months / 4
-days = weeks / 7
-print("To save up " + earningsgoal + " dollars in one year, you will need to save $" + str(round(months, 2)) + " per month.")
-print("To save up " + str(months) + " dollars in one month, you will need to save $" + str(weeks) + " per week.")
-print("To save up " + str(weeks) + " dollars in one week, you will need to save $" + str(days) + " per day.")
+# Sphere Test
+Running = True
+Balls = [1, 1]
+while Running == True :
+    Balls
+    Add = input("")
+    if Add == 1 :
+        Balls.append(1)
+    if Add == Q :
+        Running = False
+    Balls.remove()
+print(Balls)
