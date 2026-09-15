8ba9fe3e72f9b43a8ac95186fe02ffd4ce3cf183
obuen396-afk <obuen396@evit.edu>
2026-09-15 11:58:29 -0700

    Crocodile Update 9/15/26
    Got enemy to move and attack and be attacked. Boss implemented and can kill croc, but that's it. Implemented "pellets" as well.

--- Crocomys.py
+++ Crocomys.py
@@ -14,19 +14,29 @@ MAGENTA = (255,0,255)
 CYAN = (0,255,255)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
-
+GREY = (127, 127, 127)
 frames = 0
 
 Pi = 3.14
 
 font = pygame.font.Font(None, 50)
 
+seconds = 0
+
 Croc_X = X // 2
-Croc_Y = Y // 2
+Croc_Y = Y // 1.5
 Croc_Size = 25
 Croc_Color = (0,150,0)
 V_Croc_X = -2
 V_Croc_Y = 0
+Croc_Lives = 3
+Rand_Vis = 1
+Power = 0
+
+Enemy_X = 650
+Enemy_Vis = 1
+Enemy_Y = 650
+
 
 pos = pygame.mouse.get_pos( )
 x = pos[0]
@@ -42,18 +52,60 @@ while run == True :
     #Crocodile V0.0.1
     DISPLAY.fill(BLACK)
 
-    seconds = 0
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
+        Enemy_X = 650
+        Enemy_Vis = 1
+        Enemy_Y = 650
     clock = pygame.time.Clock()
     clock.tick(60)
+    def enemy_dead():
+        pass
+    def croc_power():
+        global Croc_Size
+        global Rand_Vis
+        global Power
+        global seconds
+        
+        Croc_Size = 50
+        Rand_Vis = 0
+        Power = 1
+        seconds = 1000
+    def croc_over(seconds):
+        while seconds > 0:
+            global frames
+            global Style
+            text = FONT.render("GAME OVER", True, WHITE)
+            DISPLAY.blit(text, [X // 3, Y // 2.5])
+            frames += 1
+            seconds -= 1
+            pygame.display.flip()
+        Style = 0
+    def croc_dead():
+        global Croc_Lives
+        global Croc_X
+        global Croc_Y
+        global V_Croc_X
+        global V_Croc_Y
+        Croc_Lives -= 1
+        Croc_X = X // 2
+        Croc_Y = Y // 2
+        V_Croc_X = -2
+        V_Croc_Y = 0
     def Button():
         global Style
+        global Croc_Lives
+        global seconds
+        global Rand_Vis
+        global Enemy_Vis
+        seconds = 0
+        Rand_Vis = 1
+        Enemy_Vis = 1
         Start_X = 500
         Start_Y = 150
+        Croc_Lives = 3
         pygame.draw.rect(DISPLAY, WHITE, [X // 2 - Start_X // 2, Y // 2 - Start_Y // 2, Start_X, Start_Y])
         text = font.render("PRESS SPACE BAR", True, WHITE)
         DISPLAY.blit(text, [X // 3, Y // 1.5])
@@ -63,11 +115,13 @@ while run == True :
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     Style = 2
-                elif event.key == pygame.K_0 :
-                    Level_Select.append(0)
+           #     elif event.key == pygame.K_0 :
+          #          Level_Select_List=[]
+         #           Level_Select_List.append(0)
+        #Level_Select = Level_Select_List[0] * 100 + Level_Select_List[1] * 10 + Level_Select_List[2]
 
         if Level_Select == 256:
-            Style = 1
+            Style = 2
 
     def Crocs():
         pass
@@ -78,23 +132,78 @@ while run == True :
         global Y
         global Croc_X
         global Croc_Y
+        global Enemy_X
+        global Enemy_Y
+        global Enemy_Vis
         global V_Croc_X
         global V_Croc_Y
-        Croc_Lives = 3
+        global Croc_Size
+        Rand_X = 200
+        Rand_Y = 200
+        Rand_Size = 13
+        Zoo_X = 450-300/2
+        Zoo_Y = 0
+        Zoo_Color = GREY
+        pygame.draw.rect(DISPLAY, WHITE, [0, 0, 900, 900], 25)
+        #Spawns
         if Croc_Lives > 0:
             pygame.draw.circle(DISPLAY,Croc_Color,(Croc_X,Croc_Y),Croc_Size)
-            pygame.draw.rect(DISPLAY, WHITE, [0, X, 0, Y])
-            Croc_Rect = pygame.Rect(Croc_X, Croc_Y, Croc_Size * 2, Croc_Size * 2)
-            Rectangle_Rect = pygame.Rect(60, 50, 120, 300)
-
+            if Enemy_Vis == 1:
+                pygame.draw.rect(DISPLAY, GREY, [Enemy_X, Enemy_Y, 50, 100])
+                Enemy_Coll = pygame.Rect(Enemy_X+25, Enemy_Y+25, 50, 100)
+            else:
+                Enemy_Coll = pygame.Rect(-55, -55, -55*2, -55*2)
+            if Rand_Vis == 1:
+                pygame.draw.circle(DISPLAY,YELLOW,(Rand_X,Rand_Y),Rand_Size)
+                Rand_Coll = pygame.Rect(Rand_X, Rand_Y, Rand_Size * 2, Rand_Size * 2)
+            else:
+                Rand_Coll = pygame.Rect(-55, -55, -55*2, -55*2)
+            pygame.draw.rect(DISPLAY, GREY, [Zoo_X, Zoo_Y, 300, 400])
+            Croc_Coll = pygame.Rect(Croc_X, Croc_Y, Croc_Size * 2, Croc_Size * 2)
+            Up_Coll = pygame.Rect(0, 0, 900, 50)
+            Down_Coll = pygame.Rect(0, 900, 900, 900)
+            Left_Coll = pygame.Rect(0, 0, 50, 900)
+            Right_Coll = pygame.Rect(900, 0, 900, 900)
+            Boss_Coll = pygame.Rect(Zoo_X+25, Zoo_Y+25, 300, 400)
+            
         Croc_Y += V_Croc_Y
         Croc_X += V_Croc_X
         #Thanks to _
+        
+        #Collision
+        if Croc_Coll.colliderect(Left_Coll):
+            V_Croc_X = 2
+        if Croc_Coll.colliderect(Up_Coll):
+            V_Croc_Y = 2
+        if Croc_Coll.colliderect(Right_Coll):
+            V_Croc_X = -2
+        if Croc_Coll.colliderect(Down_Coll):
+            V_Croc_Y = -2
+        if Croc_Coll.colliderect(Enemy_Coll):
+            if Power == 1:
+                Enemy_Vis = 0
+                enemy_dead()
+            else:
+                V_Croc_X = 0
+                V_Croc_Y = 0
+                croc_dead()
+        if Croc_Coll.colliderect(Rand_Coll):
+            croc_power()
+        if Croc_Coll.colliderect(Boss_Coll):
+            croc_dead()
+    
+        if seconds <= 0:
+            power = 0
+            Croc_Size = 25
 
-        if Croc_Rect.colliderect(Rectangle_Rect):
-            V_Croc_X *= -1
-            V_Croc_Y *= -1
-            Croc_Lives = 0
+        if Croc_X > Enemy_X:
+            Enemy_X += 1
+        elif Croc_X < Enemy_X:
+            Enemy_X -= 1
+        if Croc_Y > Enemy_Y:
+            Enemy_Y += 1
+        elif Croc_Y < Enemy_Y:
+            Enemy_Y -= 1
             
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -110,16 +219,6 @@ while run == True :
                 elif event.key == pygame.K_DOWN:
                     V_Croc_X = 0
                     V_Croc_Y = 2
-        def croc_over(seconds):
-            while seconds > 0:
-                global frames
-                global Style
-                text = FONT.render("GAME OVER", True, WHITE)
-                DISPLAY.blit(text, [X // 3, Y // 2.5])
-                frames += 1
-                seconds -= 1
-                pygame.display.flip()
-            Style = 0
         if Croc_Lives <= 0:
             croc_over(3000)
     if Style == 0 :
